42df0c18b31f9fe032bc4b3561743af7d66e0324
andrew-j-millward <andrew.j.millward@gmail.com>
2021-04-12 15:56:46 -0400

    Terminal input parsing

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
+import sys
 
-
-def simulate():
+def simulate(network_vector, attack_vector):
 	pass
 
 if __name__ == "__main__":
-	pass
+	args = sys.argv
+	if len(args) > 1:
+		if args[1] == "-h":
+			print("\nThe following arguments are used to specify system parameters:\n\n\tpython main.py <Diagnostics -h -v> <Number of cryptocurrencies> <Total network hashing power TH/s>\n")
+	if len(args) == 3 or len(args) == 4:
+		
+		# Valid input
+		if args[-1].isnumeric() and args[-2].isnumeric():
+			n = int(args[-2])
+			hash_power = int(args[-1])
+
+			# Even distribution of mining efforts...
+			network_vector = [1/n for i in range(n)]
+			network_hash = [hash_power*network_vector[i] for i in range(n)]
+			print(network_vector, network_hash)
+
+		# Invalid input
+		else:
+			print("Invalid input...")
