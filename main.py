a589a3d0446280cf21017b214f0175a8bb9b9c75
andrew-j-millward <andrew.j.millward@gmail.com>
2021-04-19 20:03:14 -0400

    Added payoff function and reorganized layout

--- main.py
+++ main.py
@@ -1,24 +1,46 @@
-import sys
+import sys, random
+from numpy import dot
 
-def simulate(network_vector, attack_vector):
-	pass
+############################################
+##    Compute Payoffs for Each Player     ##
+############################################
+def payoff(network_vector, attack_vector, payout_vector):
+	for i in range(len(network_vector)):
+		if attack_vector[i] > network_vector[i]:
+			return (-100, 100)
+	print(network_vector, payout_vector)
+	return (dot(network_vector, payout_vector),0)
 
+############################################
+##       Blotto Simulator Function        ##
+############################################
+def simulate(number_coins, hash_power_network, hash_power_attacker):
+	network_vector = [1/number_coins for i in range(number_coins)]
+	network_hash = [hash_power_network*network_vector[i] for i in range(number_coins)]
+	attack_vector = [0 for i in range(number_coins)]
+	attack_vector[0] = 0.01
+	difficulty_vector = [random.randrange(0,100)/100 for i in range(number_coins)]
+	print(network_vector, network_hash)
+	print(payoff(network_vector, attack_vector, difficulty_vector))
+
+############################################
+##             Input Parsing              ##
+############################################
 if __name__ == "__main__":
 	args = sys.argv
 	if len(args) > 1:
 		if args[1] == "-h":
-			print("\nThe following arguments are used to specify system parameters:\n\n\tpython main.py <Diagnostics -h -v> <Number of cryptocurrencies> <Total network hashing power TH/s>\n")
-	if len(args) == 3 or len(args) == 4:
+			print("\nThe following arguments are used to specify system parameters:\n\n\tpython main.py <Diagnostics -h -v> <Number of cryptocurrencies> <Total network hashing power TH/s> <Attacker hashing power TH/s>\n")
+	if len(args) == 4 or len(args) == 5:
 		
 		# Valid input
-		if args[-1].isnumeric() and args[-2].isnumeric():
-			n = int(args[-2])
-			hash_power = int(args[-1])
+		if args[-1].isnumeric() and args[-2].isnumeric() and args[-3].isnumeric():
+			number_coins = int(args[-3])
+			hash_power_network = int(args[-2])
+			hash_power_attacker = int(args[-1])
 
-			# Even distribution of mining efforts...
-			network_vector = [1/n for i in range(n)]
-			network_hash = [hash_power*network_vector[i] for i in range(n)]
-			print(network_vector, network_hash)
+			# Run the simulation on the given parameters...
+			simulate(number_coins, hash_power_network, hash_power_attacker)
 
 		# Invalid input
 		else:
